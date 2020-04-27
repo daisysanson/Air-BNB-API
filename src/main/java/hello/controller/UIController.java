65f142d9b3d247b6aa144515b774a6084bfd7e6c
phonologicalloop <phonologicalloop@outlook.com>
2020-04-27 12:32:25 +0100

    Find customer now returns the selected id's full info from database

--- src/main/java/hello/controller/UIController.java
+++ src/main/java/hello/controller/UIController.java
@@ -12,12 +12,14 @@ import org.springframework.web.bind.annotation.*;
 
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
+
 @RequestMapping("/")
 @Controller
 public class UIController {
-  private Customer customer;
-  private CustomerRepository repository;
-  private CustomerService customerService;
+    private Customer customer;
+    private CustomerRepository repository;
+    private CustomerService customerService;
 
     @Autowired
     public UIController(Customer customer, CustomerRepository repository, CustomerService customerService) {
@@ -27,9 +29,34 @@ public class UIController {
     }
 
 
-        @RequestMapping(value = "", method = RequestMethod.GET)
-        public String Customer(Model model) {
-            model.addAttribute("customer", customerService.getAllCustomers());
-            return "customer_information";
-        }
+//    @RequestMapping(value = "", method = RequestMethod.GET)
+//    public String showAllCustomers(Model model) {
+//        model.addAttribute("customer", customerService.getAllCustomers());
+//        return "customer_information";
+//    }
+
+
+//    @RequestMapping(value = "/index", method = RequestMethod.GET)
+//    public String getCustomerId(@RequestParam(name = "id") String id, Model model) {
+//        model.addAttribute("id", id);
+//        return "index";
+//    }
+
+    @RequestMapping(value = "/index", method = RequestMethod.GET)
+    public String showForm(Model model) {
+        customer.getId();
+
+        model.addAttribute("customer", customer);
+        return "index";
+    }
+
+    @RequestMapping(value = "/result", method = RequestMethod.POST)
+    public String showPage(@ModelAttribute("customer") Customer customer,
+                           @RequestParam("id") String id, Model model) {
+        repository.findById(id);
+        model.addAttribute("customer", customerService.selectCustomerById(id));
+        return "/result";
+
+    }
 }
+
