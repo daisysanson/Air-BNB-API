410475619795f76bd41d616ad16a9ca7ab5e18e4
phonologicalloop <phonologicalloop@outlook.com>
2020-07-09 16:16:24 +0100

    added bit on login controller to show login and added nav bar sign out

--- src/main/java/hello/controller/UIController.java
+++ src/main/java/hello/controller/UIController.java
@@ -7,6 +7,8 @@ import hello.model.Customer;
 import hello.service.CustomerService;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -25,7 +27,9 @@ public class UIController {
 
 
     @GetMapping("/index")
-    public String getCustomerId(Model model) {
+    public String getHomePage(Model model) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        model.addAttribute("loggedinuser", authentication.getName());
         return "index";
     }
 
