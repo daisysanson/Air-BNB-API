cb599ca6e932f749a06667b961b299132ec83f60
dsanson <dsanson@companieshouse.gov.uk>
2021-02-10 11:08:58 +0000

    Fixing JavaScript issues with nav and Carousel, as well as adding success message for booking confirmation

--- src/main/java/hello/config/SecurityConfig.java
+++ src/main/java/hello/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package hello.config;
 
 import hello.component.CustomAuthHandler;
 import hello.service.SiteUserDetailsService;
+import hello.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,7 +12,12 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
@@ -24,31 +30,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private CustomAuthHandler customAuthHandler;
+    
 
-
-//    @Bean
-//    public UserService mongoUserDetails() {
-//        return new UserService();
-//    }
-@Bean
-public SiteUserDetailsService mongoUserDetails() {
-    return new SiteUserDetailsService();
-}
-//    @Override
-//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        UserDetailsService userDetailsService = mongoUserDetails();
-//        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
-//
-//    }
+    @Bean
+    public SiteUserDetailsService mongoUserDetails() {
+        return new SiteUserDetailsService();
+    }
 
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-      SiteUserDetailsService siteUserDetailsService = mongoUserDetails();
+        SiteUserDetailsService siteUserDetailsService = mongoUserDetails();
         auth.userDetailsService(siteUserDetailsService).passwordEncoder(bCryptPasswordEncoder);
 
     }
-    
+
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -70,11 +66,10 @@ public SiteUserDetailsService mongoUserDetails() {
 
 
     @Override
-    public void configure(WebSecurity web) {
-        web.ignoring()
+    public void configure(WebSecurity web) throws Exception {
+        web
+                .ignoring()
                 .antMatchers("/resources/**", "/static/**", "css/css/**", "js/js/**", "/images/**", "/vendor/ **");
-
-
     }
 
 }
